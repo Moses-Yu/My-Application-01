1c50eec92cef886d16543551e33418a58371ee4f
Yu, Hyojae <mosesyumss@gmail.com>
2021-04-28 04:32:28 +0900

    RecyclerView2Activity 수정 ViewHolder 객체에 OnClickListener 등록하기

--- app/src/main/java/com/example/myapplication01/RecyclerView2Adapter.java
+++ app/src/main/java/com/example/myapplication01/RecyclerView2Adapter.java
@@ -5,15 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class RecyclerView2Adapter extends RecyclerView.Adapter<RecyclerView2Adapter.ViewHolder> {
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         TextView textView1, textView2;
 
@@ -21,12 +23,21 @@ public class RecyclerView2Adapter extends RecyclerView.Adapter<RecyclerView2Adap
             super(itemView);
             textView1 = itemView.findViewById(R.id.textView1);
             textView2 = itemView.findViewById(R.id.textView2);
+            itemView.setOnClickListener(this);
         }
 
         public void setData(int index) {
             Memo2 memo = arrayList.get(index);
             textView1.setText(memo.getTitle());
             textView2.setText(memo.getDateFormatted());
+        }
+
+        @Override
+        public void onClick(View v) {
+            int index = super.getAdapterPosition();
+            Memo2 memo = arrayList.get(index);
+            String s = String.format(Locale.KOREA,"index: %d, title: %s", index, memo.getTitle());
+            Toast.makeText(itemView.getContext(), s, Toast.LENGTH_SHORT).show();
         }
     }
 
